f01b49fba52166af12bb944f22d1c80e4712fb7d
Juan Martinez Torres <juan.d.martineztorres@outlook.com>
2019-01-28 15:47:42 -0500

    Created properties for these classes

---
+++ JournalLibrary/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JournalLibrary
+{
+    class Location
+    {
+        public int ID { get; set; }
+        public User UserID { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ JournalLibrary/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JournalLibrary
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string MI { get; set; }
+    }
+}
