1ad4a23f76fb861713721184646a5ce017d98383
Juan Martinez Torres <juan.d.martineztorres@outlook.com>
2019-01-28 22:09:24 -0500

    Created a database class, implemented an open, close, and execute method. Allowing user to open/close connection, perform insert, delete, and update function

---
+++ JournalLibrary/Database.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web;
+
+namespace JournalLibrary
+{
+    public class Database
+    {
+        string ConnectionString = "";
+        SqlConnection connection;
+
+        public void OpenConnection()
+        {
+            connection = new SqlConnection(ConnectionString);
+            connection.Open();
+        }
+        public void CloseConnection()
+        {
+            connection.Close();
+        }
+        public void ExecuteQueries(string Query_)
+        {
+            SqlCommand cmd = new SqlCommand(Query_, connection);
+            cmd.ExecuteNonQuery();
+        }           
+    }
+}
+
